7366147a9d51a6d6225b462831f760f713e77484
Rody Davis <rody.davis.jr@gmail.com>
2019-09-30 00:08:06 -0400

    Update details.dart

--- lib/ui/apps/details.dart
+++ lib/ui/apps/details.dart
@@ -1,8 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:personal_website/data/utils/constants.dart';
 
 import '../../data/classes/app.dart';
+import '../../data/utils/constants.dart';
 import '../../plugins/url_launcher/url_launcher.dart';
 
 class AppDetails extends StatefulWidget {
